fe4b3d6fc3f83ff6fb00e76248c784a88748be43
Tom Hite <thite@vmware.com>
2015-10-12 17:53:08 +0000

    fixed error in function call name
    Change-Id: Iea22dcb773b393dd28ed3da7494db5609961e835

--- does_vm_exist.py
+++ does_vm_exist.py
@@ -3,9 +3,9 @@
 import atexit
 
 try:
-	import json
+    import json
 except ImportError:
-	import simplejson as json
+    import simplejson as json
 
 try:
   from pyVim import connect
@@ -14,64 +14,64 @@ except ImportError:
   module.fail_json(msg='pyVmomi is required')
 
 def main():
-	module = AnsibleModule(
-		argument_spec=dict(
-			vcenter_host=dict(required=True, default=None),
-			vcenter_user=dict(required=True, default=None),
-			vcenter_password=dict(required=True, default=None),
-			vcenter_port=dict(required=True, type='int', default=None),
-			vm_name=dict(required=True, default=None)
-		)
-	)
+    module = AnsibleModule(
+        argument_spec=dict(
+            vcenter_host=dict(required=True, default=None),
+            vcenter_user=dict(required=True, default=None),
+            vcenter_password=dict(required=True, default=None),
+            vcenter_port=dict(required=True, type='int', default=None),
+            vm_name=dict(required=True, default=None)
+        )
+    )
 
-	vcenter_host = module.params.get('vcenter_host')
-	vcenter_user = module.params.get('vcenter_user')
-	vcenter_password = module.params.get('vcenter_password')
-	vcenter_port = module.params.get('vcenter_port')
-	vm_name = module.params.get('vm_name')
+    vcenter_host = module.params.get('vcenter_host')
+    vcenter_user = module.params.get('vcenter_user')
+    vcenter_password = module.params.get('vcenter_password')
+    vcenter_port = module.params.get('vcenter_port')
+    vm_name = module.params.get('vm_name')
 
-	try:
-		# @todo - Need to add error handler for connection timeout?
-		service_instance = connect.SmartConnect(host=vcenter_host,
-												user=vcenter_user,
-												pwd=vcenter_password,
-												port=vcenter_port)
+    try:
+        # @todo - Need to add error handler for connection timeout?
+        service_instance = connect.SmartConnect(host=vcenter_host,
+                                                user=vcenter_user,
+                                                pwd=vcenter_password,
+                                                port=vcenter_port)
 
-		atexit.register(connect.Disconnect, service_instance)
-		content = service_instance.RetrieveContent()
+        atexit.register(connect.Disconnect, service_instance)
+        content = service_instance.RetrieveContent()
 
-		children = content.rootFolder.childEntity
-		for child in children:
-			if hasattr(child, 'vmFolder'):
-				datacenter = child
-			else:
-				continue
+        children = content.rootFolder.childEntity
+        for child in children:
+            if hasattr(child, 'vmFolder'):
+                datacenter = child
+            else:
+                continue
 
-			vm_folder = datacenter.vmFolder
-			vm_list = vm_folder.childEntity
+            vm_folder = datacenter.vmFolder
+            vm_list = vm_folder.childEntity
 
-			for virtual_machine in vm_list:
-				check_vm_and_children(vm_name, virtual_machine, 1, module)
+            for virtual_machine in vm_list:
+                check_vm_and_children(vm_name, virtual_machine, 1, module)
 
-			module.exit_json(msg="Appliance does not exist.")
+            module.exit_json(msg="Appliance does not exist.")
 
-	except vmodl.MethodFault as error:
-		module.fail_json(msg="vmodl.MethodFault")
+    except vmodl.MethodFault as error:
+        module.fail_json(msg="vmodl.MethodFault")
 
 def check_vm_and_children(vm_name, virtual_machine, depth, module):
-        max_depth = 10
-        if depth > max_depth:
-                return
+    max_depth = 10
+    if depth > max_depth:
+        return
 
-        if virtual_machine.name == vm_name:
-                module.exit_json(msg="Appliance exists!")
+    if virtual_machine.name == vm_name:
+        module.exit_json(msg="Appliance exists!")
 
-        if hasattr(virtual_machine, 'childEntity'):
-                vmList = virtual_machine.childEntity
-                child_depth = depth + 1
-                for c in vmList:
-                        check_childern_vms(vm_name, c, child_depth, module)
+    if hasattr(virtual_machine, 'childEntity'):
+        vmList = virtual_machine.childEntity
+        child_depth = depth + 1
+        for c in vmList:
+            check_vm_and_children(vm_name, c, child_depth, module)
 
 from ansible.module_utils.basic import *
 if __name__ == "__main__":
-		main()
+    main()
